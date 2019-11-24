babf0884d4e7ba2129a0ff4f773a0a2268843557
laiashpeller <44324136+laiashpeller@users.noreply.github.com>
2019-11-23 17:17:41 -0800

    methods setup

--- src/main/java/cpen221/mp3/wikimediator/WikiMediator.java
+++ src/main/java/cpen221/mp3/wikimediator/WikiMediator.java
@@ -1,5 +1,7 @@
 package cpen221.mp3.wikimediator;
 
+import java.util.List;
+
 public class WikiMediator {
 
     /* TODO: Implement this datatype
@@ -14,4 +16,62 @@ public class WikiMediator {
 
      */
 
+    /**
+     *
+     * @param query
+     * @param limit
+     * @return up to limit page titles that match the query
+     */
+    List<String> simpleSearch(String query, int limit) {
+        return null;
+    }
+
+    /**
+     *
+     * @param pageTitle
+     * @return the text associated with the Wikipedia page pageTitle
+     */
+    String getPage(String pageTitle) {
+        return null;
+    }
+
+    /**
+     *
+     * @param pageTitle
+     * @param hops
+     * @return a list of page titles that can be reached by following up to hops
+     *         links starting with pageTitle
+     */
+    List<String> getConnectedPages(String pageTitle, int hops) {
+        return null;
+    }
+
+    /**
+     *
+     * @param limit
+     * @return the most common Strings used in simpleSearch and getPage.
+     *         Items are sorted in non-increasing count order.  Maximum of limit
+     *         items.
+     */
+    List<String> zeitgeist(int limit) {
+        return null;
+    }
+
+    /**
+     *
+     * @param limit
+     * @return most frequent requests in last 30 seconds.  Maximum of limit items
+     */
+    List<String> trending(int limit) {
+        return null;
+    }
+
+    /**
+     *
+     * @return maximum number of requests seen in any 30 second window.
+     *         Includes all requests made using the public API of WikiMediator
+     */
+    int peakLoad30s() {
+        return 0;
+    }
 }
