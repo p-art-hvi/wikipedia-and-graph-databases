dfcc46bcc743d2748a07dafaeff61e46d913c042
laiashpeller <44324136+laiashpeller@users.noreply.github.com>
2019-11-25 16:39:45 -0800

    4/6 methods written - not tested

--- src/main/java/cpen221/mp3/wikimediator/WikiMediator.java
+++ src/main/java/cpen221/mp3/wikimediator/WikiMediator.java
@@ -1,8 +1,10 @@
 package cpen221.mp3.wikimediator;
 
+import cpen221.mp3.cache.Cache;
+import cpen221.mp3.cache.Cacheable;
 import fastily.jwiki.core.Wiki;
 
-import java.util.List;
+import java.util.*;
 
 public class WikiMediator {
 
@@ -17,55 +19,125 @@ public class WikiMediator {
         values like null.
 
      */
+    private Wiki wiki;
+    private Map<String, List<Calendar>> requests;
+    private Set<Calendar> times;
+    private Cache<Cacheable> cache;
 
     public WikiMediator(){
-        Wiki wiki = new Wiki("en.wikipedia.org");
+        wiki = new Wiki("en.wikipedia.org");
     }
+
     /**
      *
-     * @param query
-     * @param limit
+     * @param query String to search Wikipedia for
+     * @param limit maximum number of pages to return
      * @return up to limit page titles that match the query
      */
     List<String> simpleSearch(String query, int limit) {
 
-        return null;
+        List<Calendar> dates = new ArrayList();
+        Calendar time = Calendar.getInstance();
+        if (requests.containsKey(query)) {
+            dates = requests.get(query);
+            dates.add(time);
+        }
+        requests.put(query, dates);
+        times.add(time);
+
+        return wiki.search(query, limit);
     }
 
     /**
      *
-     * @param pageTitle
+     * @param pageTitle title of page to search for
      * @return the text associated with the Wikipedia page pageTitle
      */
     String getPage(String pageTitle) {
-        return null;
+
+        Calendar time = Calendar.getInstance();
+        List<Calendar> dates = new ArrayList();
+        if (requests.containsKey(pageTitle)) {
+            dates = requests.get(pageTitle);
+            dates.add(time);
+        }
+        requests.put(pageTitle, dates);
+        times.add(time);
+
+        return wiki.getPageText(pageTitle);
     }
 
     /**
      *
-     * @param pageTitle
-     * @param hops
+     * @param pageTitle name of the page the search starts from
+     * @param hops maximum number of links allowed to follow
      * @return a list of page titles that can be reached by following up to hops
      *         links starting with pageTitle
      */
     List<String> getConnectedPages(String pageTitle, int hops) {
-        return null;
+        List<String> connectedPages = new ArrayList<>();
+        List<String> connected = wiki.getLinksOnPage(pageTitle);
+
+        int linkNumber = 0;
+        int listVal = 0;
+        if (hops == 0) {
+            return connectedPages;
+        }
+        for (String page: connected) {
+            connectedPages.add(page);
+        }
+        linkNumber++;
+
+        while (linkNumber < hops) {
+            for (int i = listVal; i < connectedPages.size(); i++ ) { //only checks new pages each time
+                List<String> newPages = wiki.getLinksOnPage(connectedPages.get(i));
+                listVal++;
+                for(String page1: newPages) {
+                    connectedPages.add(page1);
+                }
+            }
+            linkNumber++;
+        }
+
+        return connectedPages;
     }
 
     /**
      *
-     * @param limit
+     * @param limit maximum length of List to return
      * @return the most common Strings used in simpleSearch and getPage.
      *         Items are sorted in non-increasing count order.  Maximum of limit
      *         items.
      */
     List<String> zeitgeist(int limit) {
-        return null;
+
+        Map<String, Integer> common = new HashMap<>();
+        List<String> commonList = new ArrayList<>();
+        for (String page : requests.keySet()) {
+            common.put(page, requests.get(page).size());
+        }
+
+        Integer max = Integer.MIN_VALUE;
+        String mostCommon = null;
+
+        while (commonList.size() <= limit && !common.isEmpty()) {
+            max = Integer.MIN_VALUE;
+            for (String page : common.keySet()) {
+                if (common.get(page) > max) {
+                    max = common.get(page);
+                    mostCommon = page;
+                }
+            }
+            commonList.add(mostCommon);
+            common.remove(mostCommon);
+        }
+
+        return commonList;
     }
 
     /**
      *
-     * @param limit
+     * @param limit maximum length of List to return
      * @return most frequent requests in last 30 seconds.  Maximum of limit items
      */
     List<String> trending(int limit) {
