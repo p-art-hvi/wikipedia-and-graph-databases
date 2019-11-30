71aeed4ce1a2e7053ac6de3206e6279ded6a7a42
laiashpeller <44324136+laiashpeller@users.noreply.github.com>
2019-11-29 21:30:24 -0800

    RI/AFs written

--- src/main/java/cpen221/mp3/server/WikiMediatorServer.java
+++ src/main/java/cpen221/mp3/server/WikiMediatorServer.java
@@ -1,13 +1,23 @@
 package cpen221.mp3.server;
 
+import com.google.gson.JsonObject;
+
 public class WikiMediatorServer {
 
     /*
     Representation Invariant:
-    --
+    -- requests must not be null
+    -- response must not be null
+
     Abstraction Function:
-    --
+    -- request represents a JsonObject containing the id, type, and other parameters dependant on the type.
+       can contain an optional timeout
+    -- response represents a JsonObject containing the id, status, and response if the status is "success".
+       the "response" is the return value of the "type" from the request
      */
+
+    private JsonObject request;
+    private JsonObject response;
 
     /**
      * @param port the port number to bind the server to
